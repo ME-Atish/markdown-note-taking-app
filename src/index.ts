1ba15622c614843504b8c31ea6b663f1066d6a12
ME.AT <meatish2008@gmail.com>
2025-02-10 18:57:24 +0330

    feat: add structure to project

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,20 @@
-import * as mongoose from "./config/db"
-mongoose
+import * as express from "express";
+import * as cors from "cors";
+import * as dotenv from "dotenv";
+import * as multer from "multer";
+import * as marked from "marked";
+import * as mongoose from "./config/db";
+dotenv.config();
+mongoose;
+
+const app = express();
+const port = process.env.PORT || 3002;
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ massage: "welcome to my markdown note project" });
+});
+
+app.listen(port, () => {
+  console.log(`server running on http://localhost${port}`);
+});
