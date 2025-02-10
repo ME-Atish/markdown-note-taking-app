f397e6fcf3ffcb8063f98f80699fea9ffb02f8a4
ME.AT <meatish2008@gmail.com>
2025-02-10 18:59:34 +0330

    fix: fix cors err

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,16 @@ import * as cors from "cors";
 import * as dotenv from "dotenv";
 import * as multer from "multer";
 import * as marked from "marked";
+import * as fs from "fs";
 import * as mongoose from "./config/db";
 dotenv.config();
 mongoose;
 
 const app = express();
 const port = process.env.PORT || 3002;
+
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.json({ massage: "welcome to my markdown note project" });
